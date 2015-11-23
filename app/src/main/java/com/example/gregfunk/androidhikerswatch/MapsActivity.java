c1644170f51ed7de596a6c87d85c725b989777ac
Greg Funk <greg.funk@walltowall.com>
2015-11-23 15:31:28 -0500

    Play around with map marker options

--- app/src/main/java/com/example/gregfunk/androidhikerswatch/MapsActivity.java
+++ app/src/main/java/com/example/gregfunk/androidhikerswatch/MapsActivity.java
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -38,9 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.addMarker(new MarkerOptions().position(new LatLng(27.175206, 78.041533)).title("Taj Mahal").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(27.175206, 78.041533), 15));
     }
 }
